017fdd5ea10780ddba2727025405169d945f0fa3
Mohamed Abdelrazek <moh.abdelrazek2024@hotmail.com>
2024-08-19 11:01:35 +0300

    removed boilerplate

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <main className="App">
       <div>
-        <h1>Hello World!!!</h1>
+        <h1>Hello World</h1>
+        <p>{counter}</p>
+        <button onClick={() => {
+          setCounter(counter + 1)
+        }}>Increament</button>
+        <button onClick={() => {
+          setCounter(counter - 1)
+        }}>Decreament</button>
       </div>
     </main>
   )
